10541289c0cbb3b3452b0ae486637d9d93cf8ecd
ramiro-luengo <ramirobmds@gmail.com>
2022-07-12 16:10:29 -0300

    add support for joins

--- src/renderer/components/tables.tsx
+++ src/renderer/components/tables.tsx
@@ -1,4 +1,4 @@
-import Xarrow from 'react-xarrows';
+import Xarrow, { Xwrapper } from 'react-xarrows';
 
 import {
   TableMappingComponents,
@@ -12,6 +12,13 @@ interface MappedTable {
   mappings: Array<string>;
   enrichments: Array<string>;
   tableId: number;
+}
+
+interface MappedJoin {
+  name: string;
+  id: string;
+  enrichments: Array<string>;
+  drawn: boolean;
 }
 
 const extractUniqueEntityNames = (
@@ -32,10 +39,21 @@ const extractUniqueEntityNames = (
   return Array.from(uniqueTableNames);
 };
 
-const Join = (join: string, idx: number, tableIdx: number) => {
+const Join = (mappedJoin: MappedJoin) => {
+  // Set to true so we don't re-draw the join.
+  mappedJoin.drawn = true;
+
+  const { id, /* name, */ enrichments } = mappedJoin;
+
   return (
-    <div id={`join_${tableIdx}_${idx}`} className="joinContainer">
-      {join}
+    <div id={`join_${id}`} className="joinContainer">
+      {/* <div className="containerTitle">{name}</div> */}
+      <div className="containerTitle">LDM Table Enrichments:</div>
+      <ul>
+        {enrichments.map((enrichment) => (
+          <li>{enrichment}</li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -44,100 +62,131 @@ const processInstructions = (instructions: Array<TableMappingComponent>) => {
   const tableNames: Array<string> = extractUniqueEntityNames(instructions);
 
   const tables: Array<MappedTable> = [];
-  const joins: Set<string> = new Set();
+  const joins: Array<MappedJoin> = [];
   for (let i = 0; i < tableNames.length; i++) {
-    const name = tableNames[i];
+    const tableName = tableNames[i];
     const mappings: Set<string> = new Set();
-    const enrichments: Set<string> = new Set();
+    const tableEnrichments: Set<string> = new Set();
     for (let j = 0; j < instructions.length; j++) {
       const instruction = instructions[j];
       const { sourcePath, ldmField } = instruction;
 
       if (
         ldmField.includes('enrichLDMTable') &&
-        sourcePath.includes(`extractTableByName | '${name}'`)
+        sourcePath.includes(`extractTableByName | '${tableName}'`)
       ) {
         const value = ldmField.split('|').slice(1);
         value.forEach((v) => {
-          enrichments.add(v);
+          tableEnrichments.add(v);
           if (v.includes('Map')) {
             mappings.add(v);
           }
         });
       }
 
-      if (
-        sourcePath.includes('lookupAndJoinTables') &&
-        sourcePath.includes(name)
-      ) {
-        joins.add(sourcePath.split('|').slice(1).join('|'));
+      if (sourcePath.includes('lookupAndJoinTables')) {
+        // Accounts for duplicated joins.
+        const joinName = sourcePath.split('|').slice(1).join('|');
+        if (joins.every(({ name }) => joinName !== name))
+          joins.push({
+            name: joinName,
+            id: '',
+            drawn: false,
+            enrichments: ldmField.split('|').slice(1),
+          });
       }
     }
 
     tables.push({
-      name,
+      name: tableName,
       mappings: Array.from(mappings).sort(),
-      enrichments: Array.from(enrichments).sort(),
+      enrichments: Array.from(tableEnrichments).sort(),
       tableId: i,
     });
   }
 
-  return { tables, joins: Array.from(joins).sort() };
+  const processedJoins = joins.sort().map((joinValue, idx) => {
+    return {
+      name: joinValue.name,
+      id: idx,
+      enrichments: joinValue.enrichments,
+      drawn: joinValue.drawn,
+    };
+  });
+
+  return {
+    tables,
+    joins: processedJoins,
+  };
 };
 
-const Table = (tableData: MappedTable, joins?: Array<string>) => {
-  const { name, mappings, enrichments, tableId } = tableData;
+const Table = (tableData: MappedTable, joins?: Array<MappedJoin>) => {
+  const { name: tableName, mappings, enrichments, tableId } = tableData;
 
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'row',
-        columnGap: 'inherit',
+        columnGap: '14%',
       }}
     >
       <div className="tableContainer" id={`table_${tableId}`}>
-        <div style={{ fontWeight: 'bold' }}>{name}</div>
-        <div
-          style={{
-            'padding-top': '5%',
-            'text-decoration': 'underline',
-          }}
-        >
-          LDM Table Mappings:
-        </div>
+        <div style={{ fontWeight: 'bold' }}>{tableName}</div>
+        <div className="containerTitle">LDM Table Mappings:</div>
         <ul>
           {mappings.map((mapping) => (
             <li>{mapping}</li>
           ))}
         </ul>
-        <div style={{ paddingTop: '5%', textDecoration: 'underline' }}>
-          LDM Table Enrichments:
-        </div>
+        <div className="containerTitle">LDM Table Enrichments:</div>
         <ul>
           {enrichments.map((enrichment) => (
             <li>{enrichment}</li>
           ))}
         </ul>
       </div>
-      {joins ? (
-        <div className="joinsContainer">
-          {joins.map((join, idx) => Join(join, idx, tableId))}
-        </div>
+
+      {/* -------- Joins -------- */}
+      {joins?.some((join) => !join.drawn) ? (
+        <div className="joinsContainer">{joins.map(Join)}</div>
       ) : (
         ''
       )}
-      {joins
-        ? joins.map((_, idx) => (
-            <Xarrow
-              strokeWidth={1}
-              showHead={false}
-              color="white"
-              start={`table_${tableId}`}
-              end={`join_${tableId}_${idx}`}
-            />
-          ))
-        : ''}
+      {/* ------- Joins end ------- */}
+
+      {/* -------- Arrows -------- */}
+      <Xwrapper>
+        {joins
+          ? joins.map(({ name, id }) => {
+              const splittedName: Array<string> = name
+                .split('|')
+                .map((v) => v.replaceAll("'", '').trim());
+              const labelIdx: number = splittedName.indexOf(tableName) + 1;
+
+              console.log(splittedName);
+
+              return (
+                <Xarrow
+                  strokeWidth={1}
+                  showHead={false}
+                  path="straight"
+                  lineColor="white"
+                  labels={{
+                    middle: (
+                      <div style={{ color: 'white', fontSize: 11 }}>
+                        {splittedName[labelIdx]}
+                      </div>
+                    ),
+                  }}
+                  start={`table_${tableId}`}
+                  end={`join_${id}`}
+                />
+              );
+            })
+          : ''}
+      </Xwrapper>
+      {/* ------ Arrows end ------ */}
     </div>
   );
 };
@@ -158,9 +207,9 @@ const Tables = () => {
       <div className="tablesContainer">
         {tables.map((table) => {
           const tableJoins = newJoins.filter((join) =>
-            join.includes(table.name)
+            join.name.includes(table.name)
           );
-          newJoins = newJoins.filter((join) => !tableJoins.includes(join));
+          newJoins = newJoins.filter((join) => !tableJoins.includes(join.name));
           if (tableJoins) {
             return Table(table, tableJoins);
           }
